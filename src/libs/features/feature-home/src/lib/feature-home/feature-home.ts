baf24ab5aea0908228f325ac75c403c57b2640ef
Shreyas Ambre <shreyas.ambre@consultant.mannai.com.qa>
2025-12-10 11:49:53 +0530

    ngZorro Installed and integrated sample button able to load

--- src/libs/features/feature-home/src/lib/feature-home/feature-home.ts
+++ src/libs/features/feature-home/src/lib/feature-home/feature-home.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'home-feature-home',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    NzButtonModule
+  ],
   templateUrl: './feature-home.html',
   styleUrl: './feature-home.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
